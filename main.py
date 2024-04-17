15102f71415ff385b05cde241f6e1096b24a3eca
pilarmi2 <pilarmi2@fit.cvut.cz>
2024-04-17 15:22:33 +0200

    cleanup

--- main.py
+++ main.py
@@ -5,14 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": "store-service"}
