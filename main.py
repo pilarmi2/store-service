294c2110e53547cf7615e7c25f63937880cb2048
mipilar-cen64837 <mipilar@csas.cz>
2024-04-18 13:41:26 +0200

    feat: municipalities endpoints mocked

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+import uvicorn
+
+from src.routers import municipalities
 
 app = FastAPI()
+app.include_router(municipalities.router)
 
 
-@app.get("/")
-async def root():
-    return {"message": "store-service"}
+@app.get("/", include_in_schema=False, response_class=RedirectResponse)
+async def api_spec():
+    return "/docs"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
